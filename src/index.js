89020248f9085ad29e835173c62c4493dcfd0e47
limhizy15 <limhizy15@ajou.ac.kr>
2024-05-13 16:35:51 +0900

    feat: 랜덤 숫자 생성

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function determineAnswer() {
+  return Math.floor(Math.random() * 50);
+}
+
+let chance = 5;
+const log = [];
+const answer = determineAnswer();
+
+console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.');
+
+while (chance > 0) {
+  chance--;
+
+  // 입력값받고
+  // 확인해서 출력하고
+  // 지금까지 것들 보여주기
+}
