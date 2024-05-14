20b404d2f743e1eacb879ccbe8e6a5661b835c30
limhizy15 <limhizy15@ajou.ac.kr>
2024-05-14 16:36:33 +0900

    refactor: 함수 개선 - 게임 초기화, 게임 재시작 로직 분리 - 들여쓰기 2단계까지로 수정, early return으로 수정

--- src/index.js
+++ src/index.js
@@ -24,75 +24,64 @@ function readLineAsync(query) {
   });
 }
 
-/**
- * 1~50 사이 랜덤 숫자 생성
- */
 function getRandomNumber() {
-  return Math.floor(Math.random() * 50);
+  return Math.floor(Math.random() * 50) + 1;
+}
+
+async function getUserInput(promt) {
+  const input = await readLineAsync(promt);
+  // 유효성 검사
+  return Number(input);
+}
+
+function initGame() {
+  const answer = getRandomNumber();
+  let guessLog = [];
+  let attemptCount = 0;
+
+  console.log('컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.');
+
+  return { answer, guessLog, attemptCount };
 }
 
 async function restartGame() {
-  const isRestart = await readLineAsync(
+  const isRestartGame = await readLineAsync(
     '게임을 다시 시작하시겠습니까? (yes/no): '
   );
-  if (isRestart.toLowerCase() === 'yes') {
-    return true;
-  } else {
+
+  if (isRestartGame === 'no') {
     console.log('게임을 종료합니다.');
-    return false;
+    return;
   }
+
+  playGame();
 }
 
-async function play() {
-  let chance = MAX_CHANCE; // 남은 기회를 나타내는 변수
-  let log; // 사용자 입력을 저장하는 배열
-  let answer;
+async function playGame() {
+  let { answer, guessLog, attemptCount } = initGame();
 
-  while (true) {
-    if (chance === MAX_CHANCE) {
-      // 변수 초기화
-      log = [];
-      answer = getRandomNumber();
+  while (attemptCount < MAX_CHANCE) {
+    const userInput = await getUserInput('숫자 입력: ');
+    attemptCount++;
+    guessLog.push(userInput);
+
+    if (userInput === answer) {
       console.log(
-        '컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.'
+        `정답!축하합니다! ${attemptCount}번 만에 숫자를 맞추셨습니다.`
       );
+      break;
     }
 
-    chance--;
-
-    if (chance < 0) {
-      console.log(`5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`);
-
-      if (await restartGame()) {
-        chance = MAX_CHANCE;
-        continue;
-      } else break;
-    }
-
-    const userInput = await readLineAsync('숫자 입력:');
-    const userInputNumber = Number(userInput);
-
-    // 입력값 유효성 검사 필요
-
-    log.push(userInputNumber);
-
-    if (userInputNumber === answer) {
-      console.log(`정답!
-축하합니다! ${log.length}번 만에 숫자를 맞추셨습니다.`);
-      if (await restartGame()) {
-        chance = MAX_CHANCE;
-        continue;
-      } else break;
-    }
-
-    if (userInputNumber > answer) {
-      console.log('다운');
-    } else {
-      console.log('업');
-    }
-
-    console.log(`이전 추측: ${log.join(' ')}\n`);
+    console.log(userInput < answer ? '업' : '다운');
+    console.log(`이전 추측: ${guessLog.join(' ')}\n`);
   }
+
+  if (attemptCount === 5) {
+    console.log(`5회 초과! 숫자를 맞추지 못했습니다. (정답: ${answer})`);
+  }
+
+  // 정답이거나 회수를 모두 소진하여 재시작
+  restartGame();
 }
 
-play();
+playGame();
