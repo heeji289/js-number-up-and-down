af48a04b01259f21f8c83f2dac1311661fd257a1
limhizy15 <limhizy15@ajou.ac.kr>
2024-05-29 22:28:21 +0900

    test: 랜덤 숫자 생성 함수 mocking

--- src/domain/game.test.js
+++ src/domain/game.test.js
@@ -1,13 +1,18 @@
 import { Status } from '../constants';
+import { getRandomNumber } from '../utils';
 import Game from './game';
 
+jest.mock('../utils', () => ({
+  getRandomNumber: jest.fn(),
+}));
+
 describe('Game Class', () => {
-  it('init', () => {
+  it('주어진 값으로 초기화된다.', () => {
     const min = 1;
     const max = 50;
     const chance = 5;
-
     const game = new Game({ min, max, chance });
+
     expect(game.guessLog).toEqual([]);
     expect(game.attemptCount).toBe(0);
     expect(game.min).toBe(min);
@@ -18,23 +23,29 @@ describe('Game Class', () => {
   describe('playRound', () => {
     let game;
 
-    beforeEach(() => {
-      const min = 1;
-      const max = 50;
-      const chance = 5;
+    const min = 1;
+    const max = 50;
+    const chance = 5;
+    const answer = 42;
 
+    beforeEach(() => {
+      getRandomNumber.mockReturnValue(answer);
       game = new Game({ min, max, chance });
     });
 
-    it('입력이 정답과 같을 때', () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('입력이 정답과 같을 때 SUCCESS를 리턴한다.', () => {
       expect(game.playRound(game.answer)).toBe(Status.SUCCESS);
     });
 
-    it('입력이 정답보다 클 때', () => {
+    it('입력이 정답보다 클 때 UP을 리턴한다.', () => {
       expect(game.playRound(game.answer - 10)).toBe(Status.UP);
     });
 
-    it('입력이 정답보다 작을 때', () => {
+    it('입력이 정답보다 작을 때 DOWN을 리턴한다.', () => {
       expect(game.playRound(game.answer + 10)).toBe(Status.DOWN);
     });
 
